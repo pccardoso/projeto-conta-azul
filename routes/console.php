87590d8b4a1f6a860cbd9a4b12c69d9771983ca0
Paulo Cesar <paulo.cardoso2408@gmail.com>
2026-06-11 18:05:33 -0300

    master: ajustes no console.php

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
 
 Schedule::command('app:refresh-token-command')->cron('*/55 * * * *');
 
-Schedule::command('app:validate-financial-command')->cron('*/10 * * * *');
+Schedule::command('app:validate-financial-command')->cron('*/50 * * * *');
