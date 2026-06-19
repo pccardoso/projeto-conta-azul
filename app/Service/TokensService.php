3b65b2138b6d1760af490c8c6d50ec1b13c4064b
Paulo Cesar <paulo.cardoso2408@gmail.com>
2026-06-19 14:00:25 -0300

    master: adicionado ajustes no token

--- app/Service/TokensService.php
+++ app/Service/TokensService.php
@@ -14,22 +14,24 @@
         
             try{
         
-                $tokenRecord = Tokens::first();
+                $tokenRecord = Tokens::orderBy('created_at', 'asc')->get();
 
-                Log::info('Configuração Token encontrada: ' . $tokenRecord);
+                Log::info('Dados para validação de token: ' . json_encode($tokenRecord));
+
+                Log::info('*** VALIDANDO TOKEN MEU VEÍCULO');
 
                 $response = Http::asForm()
                     ->withBasicAuth(env('CONTA_AZUL_CLIENT_ID'), env('CONTA_AZUL_CLIENT_SECRET'))
                     ->post('https://auth.contaazul.com/oauth2/token', [
-                        'refresh_token' => $tokenRecord->refresh_token,
+                        'refresh_token' => $tokenRecord[0]->refresh_token,
                         'grant_type' => 'refresh_token',
                     ]);
 
-                Log::info('Resposta da API de validação de token: ' . $response->body());
+                Log::info('REPOSTA CONTA AZUL MEU VEÍCULO: ' . $response->body());
 
                 if($response->status() === 200) {
                     
-                    $tokenRecord->update([
+                    $tokenRecord[0]->update([
                         'id_token' => $response->json()['id_token'],
                         'access_token' => $response->json()['access_token'],
                         'refresh_token' => $response->json()['refresh_token'],
@@ -38,10 +40,32 @@
                     Log::error('Erro ao validar token: ' . $response->body());
                 }
 
+                Log::info('*** VALIDANDO TOKEN COBERTURA TOTAL');
+
+                $response = Http::asForm()
+                    ->withBasicAuth(env('CONTA_AZUL_CLIENT_ID_CE'), env('CONTA_AZUL_CLIENT_SECRET_CE'))
+                    ->post('https://auth.contaazul.com/oauth2/token', [
+                        'refresh_token' => $tokenRecord[1]->refresh_token,
+                        'grant_type' => 'refresh_token',
+                    ]);
+
+                Log::info('RESPOSTA CONTA AZUL COBERTURA TOTAL: ' . $response->body());
+
+                if($response->status() === 200) {
+                    
+                    $tokenRecord[1]->update([
+                        'id_token' => $response->json()['id_token'],
+                        'access_token' => $response->json()['access_token'],
+                        'refresh_token' => $response->json()['refresh_token'],
+                    ]);
+                    
+                } else {
+                    Log::error('Erro ao validar token: ' . $response->body());
+                }
+
             } catch (\Exception $e) {
                 Log::error('Erro ao validar token: ' . $e->getMessage());
             }
-
 
         }
 
